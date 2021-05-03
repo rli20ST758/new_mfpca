1b3162508d139098dc27e4e055aa19c26644b4f4
rli20ST758 <rli20@ncsu.edu>
2021-05-03 13:08:49 -0400

    fast demo

--- codes/main_fast_fast2.R
+++ codes/main_fast_fast2.R
@@ -1,4 +1,5 @@
 
+rm(list=ls())
 setwd("~/Users/new_mfpca")
 library(refund)
 library(MASS)
@@ -7,184 +8,343 @@ library(mgcv)
 library(simex)
 library(splines)
 library(rARPACK)
+library(dplyr)
 
 source("GeneData.2.R")
 source("codes/fbps.cov.R")
 source("codes/backup.R")
-source("mfpca.fast.R")
+#source("mfpca.fast.R")
 source("mfpca.fast2.R")
 source("mfpca.fast3.R")
 source("mfpca.fast4.R")
 
 
-#set.seed(2149)
-Nsub=100; J=2; L=100
-design="regular"
-data <- GeneData(I=Nsub, J=J, L=L,  design=design, level=0.95, sigma=1, balanced=FALSE)
-Y <- data$Y
-id <- data$id
 
-
-# True values
-evalues_true <- data$evalues
-eigenf_true <- data$eigenfunctions
-# Parameters
+########################################################################
+## generate multilevel functional data
+########################################################################
+I <- 100
+J <- 2
+L <- 200
 K1 <- 4
 K2 <- 4
+design <- "irregular"
+sigma <- 1
 
-
-#########################################################
-####step 1: mfpca.fast
-#########################################################
-s1 <- Sys.time()
-fit1 <- mfpca.fast(Y=Y, id=id)
-s2 <- Sys.time()
-time1 <- difftime(s2, s1, units="mins")
-
-
-# MISE of observations
-diff1 = 0
-num = 0
-for(i in 1:nrow(Y)){
-  idx = which(!is.na(Y[i, ]))
-  num = num + length(idx)
-  diff1 = diff1 + sum(abs(fit1$Yhat[i,idx]-Y[i,idx])^2)
+## create a data frame storing simulation results
+nsim <- 20
+#sim_res <- matrix(NA, nrow = nsim*2, ncol = 9)
+sim_res <- data.frame(matrix(NA, nrow = nsim*2, ncol = 8), rep(NA,nsim*2))
+colnames(sim_res) <- c("I", "J", "L", "iteration", 
+                       "comptime", "MISE(Y)", "MISE(Phi)", "MISE(Psi)", "method")
+## start simulation
+ind <- 1
+for(iter in 1:nsim){
+  set.seed(iter)
+  data <- GeneData(I = I, J = J, L = L, design = design, sigma = sigma, balanced = T)
+  Y <- data$Y
+  ## true eigenvalues and eigenfunctions
+  evalues_true <- data$evalues 
+  eigenf_true <- data$eigenfunctions
+  ## other parameters for estimation
+  id <- data$id
+  
+  ## fit MFPCA using mfpca.fast()
+  ptm <- proc.time()
+  fit_fast <- mfpca.fast2(Y = data$Y, id = id)
+  time_fast <- proc.time() - ptm
+  # MISE of observations
+  diff2 <- 0
+  num <- 0
+  for(i in 1:nrow(Y)){
+    idx = which(!is.na(Y[i, ]))
+    num = num + length(idx)
+    diff2 = diff2 + sum(abs(fit_fast$Xhat[i,idx]-Y[i,idx])^2)
+  }
+  MISE2_Y <- diff2/num
+  # MISE of eigenfucntions
+  MISE2_eigen1 <- sum(unlist(lapply(1:K1, function(x){
+    min(sum((eigenf_true[[1]][,x]-fit_fast$efunctions[[1]][,x])^2),
+        sum((eigenf_true[[1]][,x]+fit_fast$efunctions[[1]][,x])^2))})))/(K1*L)
+  MISE2_eigen2 <- sum(unlist(lapply(1:K2, function(x){
+    min(sum((eigenf_true[[2]][,x]-fit_fast$efunctions[[2]][,x])^2),
+        sum((eigenf_true[[2]][,x]+fit_fast$efunctions[[2]][,x])^2))})))/(K2*L)
+  sim_res[ind,1:8] <- round(c(I, J, L, iter, time_fast[3], MISE2_Y, MISE2_eigen1, MISE2_eigen2),4)
+  sim_res[ind,9] <- "mfpca.fast"
+  ind <- ind + 1
+  
+  ## fit MFPCA using mfpca.sc()
+  ptm <- proc.time()
+  fit_fast3 <- mfpca.fast3(Y = data$Y, id = id, weight = "obs", smooth = "separate")
+  time_sc <- proc.time() - ptm
+  # MISE of observations
+  diff1 <- 0
+  num <- 0
+  for(i in 1:nrow(Y)){
+    idx <- which(!is.na(Y[i, ]))
+    num <- num + length(idx)
+    diff1 <- diff1 + sum(abs(fit_fast3$Xhat[i,idx]-Y[i,idx])^2)
+  }
+  MISE1_Y <- diff1/num
+  # MISE of eigenfucntions
+  MISE1_eigen1 <- sum(unlist(lapply(1:K1, function(x){
+    min(sum((eigenf_true[[1]][,x]-fit_fast3$efunctions[[1]][,x])^2),
+        sum((eigenf_true[[1]][,x]+fit_fast3$efunctions[[1]][,x])^2))})))/(K1*L)
+  MISE1_eigen2 <- sum(unlist(lapply(1:K2, function(x){
+    min(sum((eigenf_true[[2]][,x]-fit_fast3$efunctions[[2]][,x])^2),
+        sum((eigenf_true[[2]][,x]+fit_fast3$efunctions[[2]][,x])^2))})))/(K2*L)
+  sim_res[ind,1:8] <- round(c(I, J, L, iter, time_sc[3], MISE1_Y, MISE1_eigen1, MISE1_eigen2),4)
+  sim_res[ind,9] <- "mfpca.fast3"
+  ind <- ind + 1
+  
+  print(iter)
+  
 }
-MISE1_Y <- diff1/num
 
-# MISE of eigenfucntions
-MISE1_eigen1 <- sum(unlist(lapply(1:K1, function(x){
-  min(sum((eigenf_true[[1]][,x]-fit1$efunctions[[1]][,x])^2),sum((eigenf_true[[1]][,x]+fit1$efunctions[[1]][,x])^2))
-})))/(K1*L)
-MISE1_eigen2 <- sum(unlist(lapply(1:K2, function(x){
-  min(sum((eigenf_true[[2]][,x]-fit1$efunctions[[2]][,x])^2),sum((eigenf_true[[2]][,x]+fit1$efunctions[[2]][,x])^2))
-})))/(K2*L)
-mpca1 <- c(time1, MISE1_Y, MISE1_eigen1, MISE1_eigen2)
+## summarise the results of nsim iterations
+sim_res_lite <- sim_res %>% 
+  group_by(I, J, L, method) %>% 
+  summarise(comptime = median(comptime), `MISE(Y)` = median(`MISE(Y)`),
+            `MISE(Phi)` = median(`MISE(Phi)`), `MISE(Psi)` = median(`MISE(Psi)`))
 
-
-
-
-#########################################################
-####step 2:  mfpca.fast2
-#########################################################
-s1 <- Sys.time()
-fit2 <-   mfpca.fast2(Y=Y, id=id)
-s2 <- Sys.time()
-time2 <- difftime(s2, s1, units="mins")
-
-
-# MISE of observations
-diff2 = 0
-num = 0
-for(i in 1:nrow(Y)){
-  idx = which(!is.na(Y[i, ]))
-  num = num + length(idx)
-  diff2 = diff2 + sum(abs(fit2$Yhat[i,idx]-Y[i,idx])^2)
-}
-MISE2_Y <- diff2/num
-
-# MISE of eigenfucntions
-MISE2_eigen1 <- sum(unlist(lapply(1:K1, function(x){
-  min(sum((eigenf_true[[1]][,x]-fit2$efunctions[[1]][,x])^2),sum((eigenf_true[[1]][,x]+fit2$efunctions[[1]][,x])^2))
-})))/(K1*L)
-MISE2_eigen2 <- sum(unlist(lapply(1:K2, function(x){
-  min(sum((eigenf_true[[2]][,x]-fit2$efunctions[[2]][,x])^2),sum((eigenf_true[[2]][,x]+fit2$efunctions[[2]][,x])^2))
-})))/(K2*L)
-mpca2 <- c(time2, MISE2_Y, MISE2_eigen1, MISE2_eigen2)
-
-
-
-
-#########################################################
-####step 3:  mfpca.fast3
-#########################################################
-s1 <- Sys.time()
-fit3 <-   mfpca.fast4(Y=Y, id=id)
-s2 <- Sys.time()
-time3 <- difftime(s2, s1, units="mins")
-
-
-# MISE of observations
-diff3 = 0
-num = 0
-for(i in 1:nrow(Y)){
-  idx = which(!is.na(Y[i, ]))
-  num = num + length(idx)
-  diff3 = diff3 + sum(abs(fit2$Yhat[i,idx]-Y[i,idx])^2)
-}
-MISE3_Y <- diff3/num
-
-# MISE of eigenfucntions
-MISE3_eigen1 <- sum(unlist(lapply(1:K1, function(x){
-  min(sum((eigenf_true[[1]][,x]-fit3$efunctions[[1]][,x])^2),sum((eigenf_true[[1]][,x]+fit3$efunctions[[1]][,x])^2))
-})))/(K1*L)
-MISE3_eigen2 <- sum(unlist(lapply(1:K2, function(x){
-  min(sum((eigenf_true[[2]][,x]-fit3$efunctions[[2]][,x])^2),sum((eigenf_true[[2]][,x]+fit3$efunctions[[2]][,x])^2))
-})))/(K2*L)
-mpca3 <- c(time3, MISE3_Y, MISE3_eigen1, MISE3_eigen2)
-
-
-
-
-#########################################################
-####step 3: mfpca.cs in refund
-#########################################################
-# s1 <- Sys.time()
-# fit3 <-  mfpca.sc(Y=Y, id=id, twoway=TRUE)
-# s2 <- Sys.time()
-# time3 <- difftime(s2, s1, units="mins")
+# ## "Smoothing failed! The code is: 52" a lot when I = 100
 # 
+# ## organize the results
+# sim_new <- as.data.frame(sim_new)
+# sim_new$method <- "fmfpca"
+# 
+# 
+# ## create a data frame storing simulation results in mfpca.sc()
+# # values <- c(100, 200, 500) ## I
+# # values <- c(100, 200) ## L
+# values <- c(3, 20, 50) ## J
+# nsim <- 50
+# sim_old <- matrix(NA, nrow = length(values)*nsim, ncol = 8)
+# colnames(sim_old) <- c("I", "J", "L", "iteration", 
+#                        "comptime", "MISE(Y)", "MISE(Phi)", "MISE(Psi)")
+# ## start simulation
+# ind <- 1
+# for(J in values){
+#   for(iter in 1:nsim){
+#     data <- GeneData(M = I, J = J, N = L, design = "regular", sigma = sigma)
+#     Y <- data$Y
+#     ## true eigenvalues and eigenfunctions
+#     evalues_true <- data$evalues 
+#     eigenf_true <- data$eigenfunctions
+#     ## other parameters for estimation
+#     id <- rep(1:I, each=J)
+#     ## fit MFPCA using mfpca.sc()
+#     ptm <- proc.time()
+#     fit1 <- mfpca.sc(Y=data$Y, id=id, twoway=TRUE)
+#     time1 <- proc.time() - ptm
+#     # MISE of observations
+#     diff1 <- 0
+#     num <- 0
+#     for(i in 1:nrow(Y)){
+#       idx <- which(!is.na(Y[i, ]))
+#       num <- num + length(idx)
+#       diff1 <- diff1 + sum(abs(fit1$Yhat[i,idx]-Y[i,idx])^2)
+#     }
+#     MISE1_Y <- diff1/num
+#     # MISE of eigenfucntions
+#     MISE1_eigen1 <- sum(unlist(lapply(1:K1, function(x){
+#       min(sum((eigenf_true[[1]][,x]-fit1$efunctions[[1]][,x])^2),
+#           sum((eigenf_true[[1]][,x]+fit1$efunctions[[1]][,x])^2))})))/(K1*L)
+#     MISE1_eigen2 <- sum(unlist(lapply(1:K2, function(x){
+#       min(sum((eigenf_true[[2]][,x]-fit1$efunctions[[2]][,x])^2),
+#           sum((eigenf_true[[2]][,x]+fit1$efunctions[[2]][,x])^2))})))/(K2*L)
+#     sim_old[ind,] <- round(c(I, J, L, iter, time1[3], MISE1_Y, MISE1_eigen1, MISE1_eigen2),4)
+#     ind <- ind + 1
+#   }
+#   print(paste0("value = ", J))
+# }
+# sim_old <- as.data.frame(sim_old)
+# sim_old$method <- "mfpca.sc"
+# 
+# sim_res <- rbind(sim_new, sim_old)
+# saveRDS(sim_res, file = "./results/regular_I100_J_L100.rds")
+# 
+# 
+# 
+# ## plot the results under different I
+# sim_res_I <- readRDS("./results/regular_I_J3_L100.rds")
+# sim_res_simp_I <- sim_res_I %>%
+#   group_by(I, J, L, method) %>%
+#   summarise(comptime = median(comptime), `MISE(Y)` = median(`MISE(Y)`),
+#             `MISE(Phi)` = median(`MISE(Phi)`), `MISE(Psi)` = median(`MISE(Psi)`))
+# 
+# plist <- list()
+# plist[["I_Time"]] <- ggplot(sim_res_simp_I) +
+#   theme_classic() +
+#   geom_line(aes(x = I, y = comptime, color = method, linetype = method)) +
+#   geom_point(aes(x = I, y = comptime, color = method)) +
+#   scale_x_continuous(breaks = c(100,500,1000,1500,2000,2500,3000)) +
+#   labs(x = "Number of subjects (I)", y = "Computing time (s)") +
+#   theme(legend.position="top", axis.title = element_text(size = 10))
+# plist[["I_MISE(Phi)"]] <- ggplot(sim_res_simp_I) +
+#   theme_classic() +
+#   geom_line(aes(x = I, y = `MISE(Phi)`, color = method, linetype = method)) +
+#   geom_point(aes(x = I, y = `MISE(Phi)`, color = method)) +
+#   ylim(0, 0.08) +
+#   scale_x_continuous(breaks = c(100,500,1000,1500,2000,2500,3000)) +
+#   labs(x = "Number of subjects (I)", y = expression(paste("MISE(", Phi, ")"))) +
+#   theme(legend.position="top", axis.title = element_text(size = 10))
+# plist[["I_MISE(Psi)"]] <- ggplot(sim_res_simp_I) +
+#   theme_classic() +
+#   geom_line(aes(x = I, y = `MISE(Psi)`, color = method, linetype = method)) +
+#   geom_point(aes(x = I, y = `MISE(Psi)`, color = method)) +
+#   ylim(0, 0.03) +
+#   scale_x_continuous(breaks = c(100,500,1000,1500,2000,2500,3000)) +
+#   labs(x = "Number of subjects (I)", y = expression(paste("MISE(", Psi, ")"))) +
+#   theme(legend.position="top", axis.title = element_text(size = 10))
+# # pdf("./results/regular_I_J3_L100.pdf", width = 9, height = 3)
+# # do.call(grid.arrange, c(plist, nrow = 1))
+# # dev.off()
+# 
+# 
+# ## plot the results under different J
+# sim_res_J <- readRDS("./results/regular_I100_J_L100.rds")
+# sim_res_simp_J <- sim_res_J %>%
+#   group_by(I, J, L, method) %>%
+#   summarise(comptime = median(comptime), `MISE(Y)` = median(`MISE(Y)`),
+#             `MISE(Phi)` = median(`MISE(Phi)`), `MISE(Psi)` = median(`MISE(Psi)`))
+# 
+# plist[["J_Time"]] <- ggplot(sim_res_simp_J) +
+#   theme_classic() +
+#   geom_line(aes(x = J, y = comptime, color = method, linetype = method)) +
+#   geom_point(aes(x = J, y = comptime, color = method)) +
+#   scale_x_continuous(breaks = c(3,20,50,100,150)) +
+#   labs(x = "Number of visits per subject (J)", y = "Computing time (s)") +
+#   theme(legend.position="top", axis.title = element_text(size = 10))
+# plist[["J_MISE(Phi)"]] <- ggplot(sim_res_simp_J) +
+#   theme_classic() +
+#   geom_line(aes(x = J, y = `MISE(Phi)`, color = method, linetype = method)) +
+#   geom_point(aes(x = J, y = `MISE(Phi)`, color = method)) +
+#   scale_x_continuous(breaks = c(3,20,50,100,150)) +
+#   ylim(0, 0.08) +
+#   labs(x = "Number of visits per subject (J)", y = expression(paste("MISE(", Phi, ")"))) +
+#   theme(legend.position="top", axis.title = element_text(size = 10))
+# plist[["J_MISE(Psi)"]] <- ggplot(sim_res_simp_J) +
+#   theme_classic() +
+#   geom_line(aes(x = J, y = `MISE(Psi)`, color = method, linetype = method)) +
+#   geom_point(aes(x = J, y = `MISE(Psi)`, color = method)) +
+#   scale_x_continuous(breaks = c(3,20,50,100,150)) +
+#   ylim(0, 0.03) +
+#   labs(x = "Number of visits per subject (J)", y = expression(paste("MISE(", Psi, ")"))) +
+#   theme(legend.position="top", axis.title = element_text(size = 10))
+# 
+# 
+# ## plot the results under different L
+# sim_res_L <- readRDS("./results/regular_I100_J3_L.rds")
+# sim_res_simp_L <- sim_res_L %>%
+#   group_by(I, J, L, method) %>%
+#   summarise(comptime = median(comptime), `MISE(Y)` = median(`MISE(Y)`),
+#             `MISE(Phi)` = median(`MISE(Phi)`), `MISE(Psi)` = median(`MISE(Psi)`))
+# 
+# plist[["L_Time"]] <- ggplot(sim_res_simp_L) +
+#   theme_classic() +
+#   geom_line(aes(x = L, y = comptime, color = method, linetype = method)) +
+#   geom_point(aes(x = L, y = comptime, color = method)) +
+#   scale_x_continuous(breaks = c(100,500,1000,1500)) +
+#   labs(x = "Dimension of the functional domain (L)", y = "Computing time (s)") +
+#   theme(legend.position="top", axis.title = element_text(size = 10))
+# plist[["L_MISE(Phi)"]] <- ggplot(sim_res_simp_L) +
+#   theme_classic() +
+#   geom_line(aes(x = L, y = `MISE(Phi)`, color = method, linetype = method)) +
+#   geom_point(aes(x = L, y = `MISE(Phi)`, color = method)) +
+#   scale_x_continuous(breaks = c(100,500,1000,1500)) +
+#   ylim(0, 0.08) +
+#   labs(x = "Dimension of the functional domain (L)", y = expression(paste("MISE(", Phi, ")"))) +
+#   theme(legend.position="top", axis.title = element_text(size = 10))
+# plist[["L_MISE(Psi)"]] <- ggplot(sim_res_simp_L) +
+#   theme_classic() +
+#   geom_line(aes(x = L, y = `MISE(Psi)`, color = method, linetype = method)) +
+#   geom_point(aes(x = L, y = `MISE(Psi)`, color = method)) +
+#   scale_x_continuous(breaks = c(100,500,1000,1500)) +
+#   ylim(0, 0.03) +
+#   labs(x = "Dimension of the functional domain (L)", y = expression(paste("MISE(", Psi, ")"))) +
+#   theme(legend.position="top", axis.title = element_text(size = 10))
+# 
+# 
+# pdf("./figures/sim_regular.pdf", width = 9, height = 9)
+# do.call(grid.arrange, c(plist, nrow = 3))
+# dev.off()
+# 
+# 
+# ## single run code
+# data <- GeneData(M = I, J = J, N = L, design = "regular", sigma = sigma)
+# Y <- data$Y
+# 
+# ## true eigenvalues and eigenfunctions
+# evalues_true <- data$evalues 
+# eigenf_true <- data$eigenfunctions
+# 
+# ## other parameters for estimation
+# id <- rep(1:I, each=J)
+# 
+# 
+# ########################################################################
+# ## fit MFPCA using different methods
+# ########################################################################
+# 
+# # ## 1. mfpca.sc
+# # ptm <- proc.time()
+# # fit1 <- mfpca.sc(Y=data$Y, id=id, twoway=TRUE)
+# # time1 <- proc.time() - ptm
+# # # MISE of observations
+# # diff1 <- 0
+# # num <- 0
+# # for(i in 1:nrow(Y)){
+# #   idx <- which(!is.na(Y[i, ]))
+# #   num <- num + length(idx)
+# #   diff1 <- diff1 + sum(abs(fit1$Yhat[i,idx]-Y[i,idx])^2)
+# # }
+# # MISE1_Y <- diff1/num
+# # # MISE of eigenfucntions
+# # MISE1_eigen1 <- sum(unlist(lapply(1:K1, function(x){
+# #   min(sum((eigenf_true[[1]][,x]-fit1$efunctions[[1]][,x])^2),
+# #       sum((eigenf_true[[1]][,x]+fit1$efunctions[[1]][,x])^2))})))/(K1*L)
+# # MISE1_eigen2 <- sum(unlist(lapply(1:K2, function(x){
+# #   min(sum((eigenf_true[[2]][,x]-fit1$efunctions[[2]][,x])^2),
+# #       sum((eigenf_true[[2]][,x]+fit1$efunctions[[2]][,x])^2))})))/(K2*L)
+# # result1 <- round(c(time1[3], MISE1_Y, MISE1_eigen1, MISE1_eigen2),4)
+# 
+# 
+# ## 2. fmfpca
+# ptm <- proc.time()
+# fit2 <- fmfpca(Y = data$Y, id = id)
+# time2 <- proc.time() - ptm
+# # print(time2)
+# 
+# ### I = 100, J = 3: L = 100 (0.3s), 500 (2.5s), 1000 (14s), 2000 (90s) slow in step 6
+# ### J = 3, L = 100: I = 100 (0.3s), 500 (1.2s), 1000 (3.7s), 2000 (15s), 5000 (105s) slow in step 4
+# ### I = 100, L = 100: J = 3 (0.3s), 20 (0.42s), 100 (1.5s), 200 (3s) slightly slow in data generation
 # 
 # # MISE of observations
-# diff3 = 0
-# num = 0
+# diff2 <- 0
+# num <- 0
 # for(i in 1:nrow(Y)){
 #   idx = which(!is.na(Y[i, ]))
 #   num = num + length(idx)
-#   diff3 = diff3 + sum(abs(fit3$Yhat[i,idx]-Y[i,idx])^2)
+#   diff2 = diff2 + sum(abs(fit2$Xhat[i,idx]-Y[i,idx])^2)
 # }
-# MISE3_Y <- diff3/num
-# 
+# MISE2_Y <- diff2/num
 # # MISE of eigenfucntions
-# MISE3_eigen1 <- sum(unlist(lapply(1:K1, function(x){
-#   min(sum((eigenf_true[[1]][,x]-fit3$efunctions[[1]][,x])^2),sum((eigenf_true[[1]][,x]+fit3$efunctions[[1]][,x])^2))
-# })))/(K1*L)
-# MISE3_eigen2 <- sum(unlist(lapply(1:K2, function(x){
-#   min(sum((eigenf_true[[2]][,x]-fit3$efunctions[[2]][,x])^2),sum((eigenf_true[[2]][,x]+fit3$efunctions[[2]][,x])^2))
-# })))/(K2*L)
-# mpca3 <- c(time3, MISE3_Y, MISE3_eigen1, MISE3_eigen2)
-
-
-
-
-################################################################
-# MISE of eigenfunctions bwtween mfpca.fast and mfpca.fast2
-################################################################
-MISE_eigen1 <- sum(unlist(lapply(1:K1, function(x){
-  min(sum((fit1$efunctions[[1]][,x]-fit2$efunctions[[1]][,x])^2),sum((fit1$efunctions[[1]][,x]+fit2$efunctions[[1]][,x])^2))
-})))/(K1*L)
-MISE_eigen2 <- sum(unlist(lapply(1:K2, function(x){
-  min(sum((fit1$efunctions[[2]][,x]-fit2$efunctions[[2]][,x])^2),sum((fit1$efunctions[[2]][,x]+fit2$efunctions[[2]][,x])^2))
-})))/(K2*L)
-
-mpca <- c(MISE_eigen1, MISE_eigen2)
-
-
-
-round(mpca1,4)
-round(mpca2,4)
-round(mpca3,4)
-#round(mpca,4)
-
-
-
-
-
-
-
-
-
-
-
+# MISE2_eigen1 <- sum(unlist(lapply(1:K1, function(x){
+#   min(sum((eigenf_true[[1]][,x]-fit2$efunctions[[1]][,x])^2),
+#       sum((eigenf_true[[1]][,x]+fit2$efunctions[[1]][,x])^2))})))/(K1*L)
+# MISE2_eigen2 <- sum(unlist(lapply(1:K2, function(x){
+#   min(sum((eigenf_true[[2]][,x]-fit2$efunctions[[2]][,x])^2),
+#       sum((eigenf_true[[2]][,x]+fit2$efunctions[[2]][,x])^2))})))/(K2*L)
+# result2 <- round(c(time2[3], MISE2_Y, MISE2_eigen1, MISE2_eigen2),4)
+# 
+# 
+# # result <- rbind(result1,result2)
+# # colnames(result) <- c("computing time", "MISE(Y)", "MISE(Phi)", "MISE(Psi)")
+# # print("The result from mfpca.sc is")
+# # print(result[1,])
+# # print("The result from fmfpca is")
+# # print(result[2,])
+# 
 
 
